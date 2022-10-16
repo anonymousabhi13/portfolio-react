6c2f25c79fbe2629ab1069ce0055d6a05589f397
Abhilash Patel <abhilashpatel687@gmail.com>
2022-10-17 04:42:55 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,210 @@
+const logotext = "Abhilash";
+const meta = {
+  title: "Abhilash Patel",
+  description:
+    "Hi! I am Ahilash Patel, a full stack web developer.Developing web applications is my passion and I love to do it.",
+};
+
+const introdata = {
+  title: "I’m Abhilash Patel",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop backend services",
+  },
+  description:
+    "Hi! I am a student of computer science engineering at Lakshmi Narain college of technology and science.I am a Mern Stack Developer, self learner and moving ahead to learn something amazing and extraordinary.",
+  your_img_url: "https://images.unsplash.com/photo-1596003906949-67221c37965c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+};
+
+const dataabout = {
+  title: "Something about my self",
+  aboutme:
+    "Hi! I am a student of computer science engineering at Lakshmi Narain college of technology and science.I am a Mern Stack Developer, self learner and moving ahead to learn something amazing and extraordinary.",
+};
+const worktimeline = [
+  {
+    jobtitle: "October",
+    where: "Instagram Automation",
+    date: "OCT,2022",
+  },
+  {
+    jobtitle: "September",
+    where: "Socket io chat app",
+    date: "Sept,2022",
+  },
+  {
+    jobtitle: "August",
+    where: "Mailing App",
+    date: "Aug,2022",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "Nodejs",
+    value: 85,
+  },
+
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "Express",
+    value: 85,
+  },
+  {
+    name: "java",
+    value: 80,
+  },
+  {
+    name: "Redux",
+    value: 80,
+  },
+];
+
+const services = [
+  {
+    title: "Glass Morphism Creator",
+    description:
+      "I made a glass morphism creator which can be used to create glass morphism effect on any element.You just have to copy and paste the values in your css file.",
+  },
+  {
+    title: "Let's_talk",
+    description:
+      "I made a chat app using socket.io and react.I used Node js and Socket io to make the app.",
+  },
+  {
+    title: "Video-Calling-app",
+    description:
+      "I made a video calling app using socket.io and Webrtc.I used Node js,Socket io and Webrtc to make the app.",
+  },{
+    title: "Keep Notes app",
+    description:
+      "I made a keep notes app using React .I used React and react hooks to manage the state of app.",
+  },{
+    title: "Weather Handler app",
+    description:
+      "I made a Weather app using React .I used React and openWeather API and React hooks to manage the state of app.",
+  },
+  {
+    title: "Social Media app",
+    description:
+      "I made a Social Media app using Nodejs .I used Node js,passport js,mongoDB,Multer,Nodemailer to make the app.",
+  }, {
+    title: "Responsive Template",
+    description:
+      "I made a Responsive Template using HTML,CSS,JS .I used GSAP to make the animation of the template.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://markodenic.com/wp-content/uploads/2021/05/glassmorphism-generator2-e1620900331190.png",
+    desctiption:
+      "I made a glass morphism creator which can be used to create glass morphism effect on any element.You just have to copy and paste the values in your css file.",
+    link: "https://glassmorphism-creator.netlify.app/",
+  },
+  {
+    img: "https://www.mindinventory.com/blog/wp-content/uploads/2017/07/chat-app.jpg",
+    desctiption:
+      "I made a chat app using socket.io and react.I used Node js and Socket io to make the app.",
+    link: "https://letsdoshit-01.herokuapp.com/",
+  },
+  {
+    img: "https://www.mirrorfly.com/blog/wp-content/uploads/2022/02/Best-group-video-call-app.png",
+    desctiption:
+      "I made a video calling app using socket.io and Webrtc.I used Node js,Socket io and Webrtc to make the app.",
+    link: "https://video-chat-app-prac.netlify.app/",
+  },
+  {
+    img: "https://images.ctfassets.net/lzny33ho1g45/IQ3BZdqRidr3lTsXd0Qk9/08275a6b34eedf192169088382f68f20/Best_email_apps_-_image7.png",
+    desctiption:
+      "I made a mailing app using Nodejs .I used Node js,passport js,mongoDB,Multer,Nodemailer to make the app.",
+    link: "https://github.com/anonymousabhi13/GmailCloneUsingMEN",
+  },
+  {
+    img: "https://miro.medium.com/max/1200/0*Dl_7-c1zhD21TY5b.gif",
+    desctiption:
+      "I made a keep notes app using React .I used React and react hooks to manage the state of app.",
+    link: "https://notes-making-app-wr.netlify.app/",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVo2xYDi9O7RQQo7J2-roejFmbhV7Glt0KIA&usqp=CAU",
+    desctiption:
+      "I made a Weather app using React .I used React and openWeather API and React hooks to manage the state of app.",
+    link: "https://weather-handler.netlify.app/",
+  },
+
+  {
+    img: "https://cdn.dribbble.com/users/3981241/screenshots/14530848/instagram_2_4x.png",
+    desctiption:
+      "I made a Social Media app using Nodejs .I used Node js,passport js,mongoDB,Multer,Nodemailer to make the app.",
+    link: "https://instagram-app-01.herokuapp.com/",
+  },
+  {
+    img: "https://s3.amazonaws.com/blog.invisionapp.com/uploads/2018/02/slack-responsive-web-design.png",
+    desctiption:
+      "I made a Responsive Template using HTML,CSS,JS .I used GSAP to make the animation of the template.",
+    link: "https://spaincollectionclone.netlify.app/",
+  },
+  {
+    img: "https://i.pinimg.com/originals/37/84/c9/3784c957ea790f4593623b1f90c349eb.jpg",
+    desctiption:
+      "I Created a Payment app using Razorpay API .I used Razorpay API to make the app.",
+    link: "https://github.com/anonymousabhi13/Payment_gateway",
+  },
+  {
+    img: "https://cdn.dribbble.com/users/5031392/screenshots/16844064/dribbble_shot_-_sales_call_analyser___23.08___eugene_k._anastasia_sergienko.png?compress=1&resize=400x300",
+    desctiption:
+      "I Created Text-to-speech app using Nodejs .I used Node js,passport js,mongoDB,npm SAY to make the app.",
+    link: "https://github.com/anonymousabhi13/Text-to-Speech_node-js",
+  },
+  {
+    img: "https://cdn.dribbble.com/users/1446436/screenshots/3516840/mmm_home.png?compress=1&resize=400x300",
+    desctiption:
+      "I Created TMDB clone using React .I used React and TMDB API to make the app.",
+    link: "https://tmbd-clone-app.netlify.app/",
+  },
+  {
+    img: "https://cdn.dribbble.com/users/3860505/screenshots/14923736/media/f60a9efafb026a35f79698ea19a81ad9.png?compress=1&resize=400x300",
+    desctiption:
+      "Under Development",
+    link: "https://github.com/anonymousabhi13",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "abhilashpatel155@gmail.com",
+  YOUR_FONE: "+918817836116",
+  description: "FOR ANY QUERIES OR SUGGESTIONS PLEASE CONTACT ME",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/anonymousabhi13",
+  facebook: "https://facebook.com/",
+  linkedin: "https://www.linkedin.com/in/abhilash-patel-731585203",
+  twitter: "https://app.netlify.com/teams/anonymousabhi13/overview",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
